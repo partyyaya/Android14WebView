23511cc4c67ebe874d772af089aa05c0be163f6a
LBJ <LBJ>
2017-08-20 12:20:26 +0800

    20170820

--- app/src/main/java/tw/ming/app/helloworid/mywebview/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mywebview/MainActivity.java
@@ -1,5 +1,13 @@
 package tw.ming.app.helloworid.mywebview;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +19,78 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private EditText name;
+    private LocationManager lmgr;
+    private MyListener myListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lmgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        myListener = new MyListener();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }else{
+            init();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+
+    private void init(){
         webView = (WebView)findViewById(R.id.webview);
         name = (EditText)findViewById(R.id.name);
         initWebView();
+        lmgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,myListener);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private class MyListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        lmgr.removeUpdates(myListener);
     }
     private void initWebView(){
         webView.setWebViewClient(new WebViewClient());//將網頁包在app裡面
+
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);//將javascript打開
 
@@ -29,8 +99,9 @@ public class MainActivity extends AppCompatActivity {
     }
     public void test1(View view) {
         //webView.goBack();//返回上一個網頁
-        String myname = name.getText().toString();
-        webView.loadUrl("javascript:test3("+myname+")");//執行此網頁的test3方法
+        //String myname = name.getText().toString();
+        //webView.loadUrl("javascript:test3("+myname+")");//執行此網頁的test3方法
+        webView.loadUrl("javascript:goto(23,123)");
     }
 
     public void test2(View view) {
