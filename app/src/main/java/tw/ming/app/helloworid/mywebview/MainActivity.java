b9e26d0dfdf87adc53e043801248baed33e4f7ab
LBJ <LBJ>
2017-08-20 13:50:06 +0800

    20170820

--- app/src/main/java/tw/ming/app/helloworid/mywebview/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mywebview/MainActivity.java
@@ -8,9 +8,12 @@ import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -21,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText name;
     private LocationManager lmgr;
     private MyListener myListener;
+    private MyJS myJS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        myJS = new MyJS();
         lmgr = (LocationManager) getSystemService(LOCATION_SERVICE);
         myListener = new MyListener();
 
@@ -64,7 +68,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onLocationChanged(Location location) {
-
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            webView.loadUrl("javascript:goto("+lat+","+lng+")");
+            //可按模擬器右下3點點send寄送位置改變地點
         }
 
         @Override
@@ -93,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);//將javascript打開
+        webView.addJavascriptInterface(myJS,"ming");//將類別傳給javascript,類別名叫:ming
 
         webView.loadUrl("file:///android_asset/ming01.html");//asset代表一個檔案沒加s
         //第三個/代表根目錄
@@ -114,5 +122,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void test4(View view) {
 
+    }
+    private class MyJS{
+        @JavascriptInterface //要給javascript用
+        public void m1(String name){
+            Log.i("ming","MyJS"+name);
+        }
+
+        @JavascriptInterface
+        public void alert(String mesg){
+            AlertDialog dialog = null;
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Mesg");
+            builder.setMessage(mesg);
+            dialog = builder.create();
+            dialog.show();
+        }
     }
 }
